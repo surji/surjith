07ae967d5f53090feedd71b95b9273b06a6f8840
surji <surjithkumarcse@gmail.com>
2016-12-29 22:46:05 -0800

    Add files via upload

---
+++ calculate n numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,sum=0;
+printf("\n enter the numbers");
+scanf("%d",&n);
+for(i=0;i<=n;i++){
+sum=sum+i;
+}
+printf("\nsum=%d",sum);
+}
+
+
